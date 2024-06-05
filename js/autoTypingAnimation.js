52bb44fe6a75fbc89903330645c57254cbd0b6bf
Vajja Premsai <108271437+Vajja-Premsai@users.noreply.github.com>
2024-06-05 12:01:22 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,13 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Vajja Premsai</span><br> I am passionate about crafting innovative and user-friendly web solutions that make a positive impact on people's lives. I am skilled in creating dynamic and responsive websites that are optimized for performance and usability. <br> I use HTML, CSS, javaScript, Bootstrap, ReactJS, PHP and MySQL to build web sites. Currently holding the responsibility of <span>Web-Lead </span> at ACM GMRIT. "
+      
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
+
+
+
